5a13898b4ee7c6ec88a15e90179c99f92fba6db3
jojomaverik <arda.bizeldodo@gmail.com>
2024-07-22 12:24:06 +0300

    Revert "New Update on Ardunio code"
    This reverts commit 1415d6f48feba72117adf55cfeb8bd06b47cb24c.

--- CarAvoidingObjects_v2.ino
+++ CarAvoidingObjects_v2.ino
@@ -1,5 +1,5 @@
-#define echoPin 12 // Ultrasonic sensor 
-#define trigPin 11 // Ultrasonic sensor
+#define echoPin 13 // Ultrasonic sensor 
+#define trigPin 12 // Ultrasonic sensor
 #define MotorR1 7 // IN1 on L298N
 #define MotorR2 6 // IN2 on L298N
 #define MotorRenable 9  // enA on L298N
@@ -8,6 +8,7 @@
 #define MotorLenable 10 // enB on L298N
 
 long distance, time;
+bool hasMovedBackward = false; // flag
 
 void setup() {
   pinMode(echoPin, INPUT);
@@ -19,7 +20,7 @@ void setup() {
   pinMode(MotorR1, OUTPUT);
   pinMode(MotorR2, OUTPUT);
   pinMode(MotorRenable, OUTPUT);
-  
+
   Serial.begin(9600);
 }
 
@@ -37,49 +38,45 @@ void loop() {
   Serial.print("Distance: ");
   Serial.println(distance);
 
-  if (distance < 15) {
+ if (distance < 15) {
+    if(!hasMovedBackward){
     moveBackward();
-    delay(1000);
+    delay(150);
+    hasMovedBackward = true;
+    }
     turnRight();
-    delay(800);
+    delay(500);
+    moveForward();
+    delay(1000);
+    turnLeft();
+    delay(500);
   }
   else {
+    hasMovedBackward = false;
     moveForward();
-  } 
+  }
 }
 
 void moveForward() {
   digitalWrite(MotorR1, HIGH);
   digitalWrite(MotorR2, LOW);
-  analogWrite(MotorRenable, 115);
-
-  digitalWrite(MotorL1, HIGH);
-  digitalWrite(MotorL2, LOW);
-  analogWrite(MotorLenable, 100);
-}
-/*
-void stop() {
-  digitalWrite(MotorR1, HIGH);
-  digitalWrite(MotorR2, LOW);
-  analogWrite(MotorRenable, 0);
-
-  digitalWrite(MotorL1, HIGH);
-  digitalWrite(MotorL2, LOW);
-  analogWrite(MotorLenable, 0);
-
-  delay(4000);
-}*/
-
-void turnRight() {
-  digitalWrite(MotorR1, LOW);
-  digitalWrite(MotorR2, HIGH);
   analogWrite(MotorRenable, 100);
 
   digitalWrite(MotorL1, HIGH);
   digitalWrite(MotorL2, LOW);
   analogWrite(MotorLenable, 100);
 }
-/*
+
+void turnRight() {
+  digitalWrite(MotorR1, HIGH);
+  digitalWrite(MotorR2, LOW);
+  analogWrite(MotorRenable, 0);
+
+  digitalWrite(MotorL1, HIGH);
+  digitalWrite(MotorL2, LOW);
+  analogWrite(MotorLenable, 100);
+}
+
 void turnLeft() {
   digitalWrite(MotorR1, HIGH);
   digitalWrite(MotorR2, LOW);
@@ -88,7 +85,7 @@ void turnLeft() {
   digitalWrite(MotorL1, HIGH);
   digitalWrite(MotorL2, LOW);
   analogWrite(MotorLenable, 0);
-} */
+}
 
 void moveBackward() {
   digitalWrite(MotorR1, LOW);
